a8baa8127efa17fc3df108ddc683e838e1f1c7f3
bo xiong <paul.xiong.2007@gmail.com>
2011-09-14 17:00:31 -0700

    add missed direcotry working-copy

--- galaxytab101_build_kernel.sh
+++ galaxytab101_build_kernel.sh
@@ -2,7 +2,7 @@
 #!/bin/sh
 
 export ARCH=arm
-export CROSS_COMPILE=/home/bxiong/android/GT-P7510_from_Supercurio/arm-2010.09/bin/arm-none-eabi-
+export CROSS_COMPILE=/home/boxiong/honeycomb/galaxy_gt-p7510/arm-2010.09/bin/arm-none-eabi-
 export LOCALVERSION=""
 
 export KBUILD_BUILD_VERSION="PXiong"
